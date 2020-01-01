50188d27deaec713a7c8ecf829152bd64347869e
Amanda Rolon <abrolon87@gmail.com>
2020-01-01 16:46:07 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,12 +1,12 @@
 # Your code goes here!
 class Anagram
   attr_accessor :word
-  def initialize
-
+  def initialize(word)
+    @word = Array.new(word)
   end
 
   def match(word)
-    @word = Array.new(word)
+
 
   end
 end
